3058bbbcc0d444f9a3598538a5b2df27238ad5be
James Byrne <jamesdesbyrne@gmail.com>
2019-01-31 17:01:36 +0000

    Adding the searchForCustomers function to the customer-search component
    This function will be in charge of retrieving the list of customers as
    the user types them. We are setting this up as a task so we can debounce
    it but also avoid any rendering issues later on if the component was to
    be removed before the function evaluated.

--- app/components/customer-search.js
+++ app/components/customer-search.js
@@ -1,4 +1,24 @@
 import Component from '@ember/component';
+import { task, timeout } from 'ember-concurrency';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
+  store: service(),
+
+  searchForCustomers: task(function * (searchTerm) {
+    // Check that the term is not empty
+    if (searchTerm === '' || searchTerm === undefined) return;
+
+    // Delay the request by 300 milliseconds
+    // incase the user is still typing
+    yield timeout(300);
+
+    try {
+      const result = yield this.store.query('customer', {filterTerm: searchTerm});
+      this.set('customers', result);
+    } catch(e) {
+      console.log(e);
+      // TODO: show a error message
+    }
+  }).restartable(),
 });

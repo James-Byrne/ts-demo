c599277b3d64fb02a220116ee18d8e26e0e5ae89
James Byrne <jamesdesbyrne@gmail.com>
2019-02-01 17:01:32 +0000

    Adding keyboard navigation and customer selection/clearing
    Part one of this will add the customers Last and First names to the
    button inplace of the input field. Part two will be showing a tooltip
    and allowing the user to select the customer for editing.
    
    Allows the user to move up and down the list of customers using their
    arrow keys and allows enter to be used to select a customer

--- app/components/customer-search.js
+++ app/components/customer-search.js
@@ -2,8 +2,64 @@ import Component from '@ember/component';
 import { task, timeout } from 'ember-concurrency';
 import { inject as service } from '@ember/service';
 
+// Helpers for keeping track of the
+// keyboard events
+const UP = 38;
+const DOWN = 40;
+const ENTER = 13;
+
 export default Component.extend({
   store: service(),
+  focusElement: -1,
+  customers: null,
+
+  didInsertElement() {
+    this._super(...arguments);
+
+    // Get a reference to both the customer search component
+    const customerSearch = document.querySelector('#customer-search');
+
+    customerSearch.addEventListener('mouseleave', () => {
+      // Get a reference to the customerInput, we need to do this here
+      // incase it has ever been hidden which would break our reference
+      const customerInput = document.querySelector('#customer-search-input');
+
+      // If the mouse leaves the customer search component and the
+      // customer search input is not focused then hide the dropdown
+      if (customerInput != document.activeElement) this.set('showDropdown', false);
+    });
+  },
+
+  // Extend the keyDown event on our component
+  // Here we want to allow the user to navigate the
+  // dropdown menu with the up and down arrows.
+  //
+  // We also want to trigger the setSelectedCustomer
+  // action when the user has selected a customer and
+  // presses the enter key
+  keyDown(e) {
+    this._super(...arguments);
+
+    if (this.showDropdown) {
+      switch(e.keyCode) {
+        case UP:
+          if (this.focusElement > 0) this.decrementProperty('focusElement');
+          break;
+        case DOWN:
+          if (this.customers && (this.focusElement + 1) < this.customers.length) {
+            this.incrementProperty('focusElement');
+          }
+          break;
+        case ENTER:
+          if (this.focusElement >= 0) {
+            this.setSelectedCustomer(this.customers.objectAt(this.focusElement));
+          }
+          break;
+        default:
+            return
+      }
+    }
+  },
 
   searchForCustomers: task(function * (searchTerm) {
     // Check that the term is not empty
@@ -17,8 +73,23 @@ export default Component.extend({
       const result = yield this.store.query('customer', {filterTerm: searchTerm});
       this.set('customers', result);
     } catch(e) {
-      console.log(e);
       // TODO: show a error message
     }
   }).restartable(),
+
+  setSelectedCustomer(customer) {
+    this.set('selectedCustomer', customer);
+    this.set('showDropdown', false);
+  },
+
+  actions: {
+    setSelectedCustomer(customer) {
+      this.setSelectedCustomer(customer);
+    },
+
+    clearCustomer() {
+      this.set('selectedCustomer', null);
+      this.set('searchTerm', '');
+    }
+  },
 });

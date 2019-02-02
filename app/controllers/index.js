a6a62ffa609049393995ee3df6eb7e1ef849f22b
James Byrne <jamesdesbyrne@gmail.com>
2019-02-02 13:48:46 +0000

    Initial setup for showing & hiding the customer form modal
    As per the requirements (and I believe good practice in this case) I
    will be passing down two actions to the cusotmer-search component. The
    first action will be used to create a new customer and open the customer
    details form. The later will take a customer and poulate their details
    into the customer details form for editing.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,4 +1,23 @@
 import Controller from '@ember/controller';
+import { openModal, closeModal } from '../lib/utils';
 
 export default Controller.extend({
+  openModal,
+  closeModal,
+
+  actions: {
+    openModal,
+    closeModal,
+
+    editCustomer(customer) {
+      this.set('customer', customer);
+      this.openModal('CustomerDetailsModal');
+    },
+
+    createCustomer() {
+      const newCustomer = this.store.createRecord('customer');
+      this.set('customer', newCustomer);
+      this.openModal('CustomerDetailsModal');
+    },
+  }
 });

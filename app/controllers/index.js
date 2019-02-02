d7fc12a85ffda8f43c21d81beede064ec61ab033
James Byrne <jamesdesbyrne@gmail.com>
2019-02-02 15:37:39 +0000

    Index Controller: Using a constant for the modal, adding saveCustomer task and rolling back any unsaved changes when the modal closes
    Using a constant allows us to change the underlying string later if
    needed.
    
    Persist a new customer or the changes we have made to an existing
    customer to the mirage database.
    
    If the user closes the modal we want them to rollback any changes that
    they may have made. This ensures that the state of the app does not fall
    out of sync with the database or surprise the user.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,6 +1,9 @@
 import Controller from '@ember/controller';
-import { openModal, closeModal } from '../lib/utils';
 import { inject as service } from '@ember/service';
+import { task } from 'ember-concurrency';
+import { openModal, closeModal } from '../lib/utils';
+
+const CUSTOMER_DETAILS_MODAL = 'CustomerDetailsModal';
 
 export default Controller.extend({
   store: service(),
@@ -8,19 +11,30 @@ export default Controller.extend({
   openModal,
   closeModal,
 
+  saveCustomer: task(function * (customer) {
+    try {
+      yield customer.save();
+      this.closeModal(CUSTOMER_DETAILS_MODAL);
+    } catch(e) {
+      // show an error message
+    }
+  }),
+
   actions: {
-    openModal,
-    closeModal,
+    closeModal(modal) {
+      this.closeModal(modal);
+      this.customer.rollbackAttributes();
+    },
 
     editCustomer(customer) {
       this.set('customer', customer);
-      this.openModal('CustomerDetailsModal');
+      this.openModal(CUSTOMER_DETAILS_MODAL);
     },
 
     createCustomer() {
       const newCustomer = this.store.createRecord('customer');
       this.set('customer', newCustomer);
-      this.openModal('CustomerDetailsModal');
+      this.openModal(CUSTOMER_DETAILS_MODAL);
     },
   }
 });
